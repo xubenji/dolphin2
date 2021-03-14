a99599c1e573cb87a2e57094304da0923ba88bdd
XU, yanjie <459547070@qq.com>
2021-03-14 13:19:04 +0800

    Update audio.c

--- kernel/drives/audio/audio.c
+++ kernel/drives/audio/audio.c
@@ -3,6 +3,7 @@
 
 int audio(){
     printf("This is the problem place:\n");
+    printf("When you audio_drive.h the follow var won't change:\n");
     printf("%s\n",test);
     printf("I am audio drive, and I need =>");
     audio_aarch();
