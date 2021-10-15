36182ab174eb784c42b5da684531e73873955ece
xubenji <459547070@qq.com>
2021-10-15 17:19:59 -0400

    快速转化架构脚本

--- switcharch.sh
+++ switcharch.sh
@@ -1,15 +1,15 @@
+#转化平台，想要转为x86平台，就输入
+#bash switcharch.sh x86
+#转为arm平台请输入
+#bash switcharch.sh arm
+
 if [ "$1" == "x86" ]
     then
 sed -i "s/arm/x86/g" config.mk
 sed -i "s/arm/amd64/g" ./.vscode/launch.json
 make clean
 
-if [ "$2" == "d" ]
-then
 make $2
-else 
-make run
-fi
 
 elif [ "$1" == "arm" ]
     then
@@ -17,20 +17,10 @@ sed -i "s/x86/arm/g" config.mk
 sed -i "s/amd64/arm/g" ./.vscode/launch.json
 make clean
 
-if [ "$2" == "d" ]
-then
 make $2
-else 
-make run
-fi
 
 else 
 
-if [ "$2" == "d" ]
-then
 make $2
-else 
-make run
-fi
 
 fi
