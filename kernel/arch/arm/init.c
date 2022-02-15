9c0e31d033597882f454f59a0745291fcae6df64
xubenji <459547070@qq.com>
2022-02-15 06:55:14 +0000

    added the timer handler

--- kernel/arch/arm/init.c
+++ kernel/arch/arm/init.c
@@ -7,5 +7,12 @@ void init_all(){
     init_uart();
     printk("test!!!!!");
     printk("We current at level: %d\n", (uint64_t)get_el());
+    init_timer();
+    enable_irq();
+    while (1)
+    {
+        /* code */
+    }
+    
 }
 
